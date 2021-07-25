1b6ce7733c96cd11ba6c3aa62f6f2d6484bbac15
wbbme <100@wbb.me>
2021-07-25 21:20:34 +0800

    change error

--- components/Footer.js
+++ components/Footer.js
@@ -6,8 +6,9 @@ const Footer = ({ fullWidth }) => {
   const from = +BLOG.since
   return (
     <div
-      className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${!fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
-        }`}
+      className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
+        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
+      }`}
     >
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6 font-medium">
