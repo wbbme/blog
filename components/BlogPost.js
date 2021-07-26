f03fc0469a54c33090bce8aa4be6d242fe2c3e7f
Bolby Wang <100@wbb.me>
2021-07-26 18:57:38 +0800

    change Description opacity

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -18,7 +18,7 @@ const BlogPost = ({ post }) => {
         </time>
       </header>
       <main>
-        <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
+        <p className="hidden md:block leading-8 opacity-70 text-gray-700 dark:text-gray-300">
           {post.summary}
         </p>
       </main>
