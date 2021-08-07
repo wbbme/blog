7b7d6c4b082d95f730014012e69c9f851f33dca6
Bolby Wang <100@wbb.me>
2021-08-08 02:23:44 +0800

    样式修复

--- components/Container.js
+++ components/Container.js
@@ -70,7 +70,7 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
           fullWidth={fullWidth}
         />
         <main
-          className={`m-auto flex-grow transition-all ${
+          className={`m-auto w-full flex-grow transition-all ${
             !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
           }`}
         >
