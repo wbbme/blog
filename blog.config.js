5251913672eb443c8312cb70674cf5bd54b64e76
wbbme <100@wbb.me>
2023-09-10 16:16:13 +0800

    :construction: change comment

--- blog.config.js
+++ blog.config.js
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '1b9f5ee1-82fd-4c3b-b5a7-635ea923e7bb', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
